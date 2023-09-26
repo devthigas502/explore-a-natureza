3ff849ab72cff0397f6f9ef196d12289bb7edb66
natinhaaa <natvcer2@gmail.com>
2023-09-26 08:43:08 -0300

    update

--- js/index.js
+++ js/index.js
@@ -1,24 +1,24 @@
 const imagens = document.querySelectorAll('.js-list li')
-const textos = document.querySelectorAll('.js-conteudo section')
-const pergunta = document.querySelectorAll('.js-faq section')
+const textos = document.querySelectorAll('.js-conteúdo nav')
+const pergunta = document.querySelectorAll('.js-faq h3')
 const resposta = document.querySelectorAll('.js-faq p')
 
 console.log(imagens)
 console.log(textos)
 
 
-function ativarConteudo(indice) {
-    textos.forEach((item) => {
-        item.classList.remove('active')
-    })
-    textos[indice].classList.add('active')
-}
+// function ativarConteudo(indice) {
+//     textos.forEach((item) => {
+//         item.classList.remove('active')
+//     })
+//     textos[indice].classList.add('active')
+// }
 
-imagens.forEach((item,indice) => {
-    item.addEventListener('click',() => {
-        ativarConteudo(indice)
-    })
-})
+// imagens.forEach((item, indice) => {
+//     item.addEventListener('click',() => {
+//         ativarConteudo(indice)
+//     })
+// })
 /*-------------------------------------------------------------*/
 
 function ativarFaq(indice) {
