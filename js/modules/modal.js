75bf377943d9afcae8f9117e62a4549b66f79dec
natinhaaa <natvcer2@gmail.com>
2023-10-16 18:37:20 -0300

    update

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,18 +1,35 @@
 export default function modal(){
 
-    const form = document.querySelector('.form')
+    const modal = document.querySelector(".js-modal");
     const dados = {}
+    if (localStorage.getItem("email") === null) {
+        modal.classList.add("active");
+    }
+    else {
+        modal.classList.remove("active");
+    }
 
     function pegarValorForm(event){
         dados[event.target.name] = event.target.value
         console.log(dados)
     }
 
-    form.addEventListener('change', pegarValorForm)
-    form.addEventListener('submit', (event) => {
-        event.preventDefault()
-        localStorage.setItem(dados.email, JSON.stringify(dados))
+    modal.addEventListener('change', pegarValorForm)
+    document.querySelector(".modal-forms").addEventListener("submit", (event) => {
+        event.preventDefault();
+        localStorage.setItem("email", JSON.stringify(dados.email))
         alert("Dados salvos com sucesso")
-    })
+        modal.classList.remove("active");
+    });
+
+    document.querySelector(".modal-close").addEventListener("click", () => {
+        modal.classList.remove("active");
+    });
+
+    if (localStorage.getItem("email") === null) {
+        setTimeout(() => {
+            modal.classList.add("active");
+        }, 3000);
+      }
 
 }
